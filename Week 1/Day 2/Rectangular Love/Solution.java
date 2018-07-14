bbeab969fca027357f988386739817471b3593dc
snigdha-rao <37892057+snigdha-rao@users.noreply.github.com>
2018-07-14 15:13:52 +0530

    Add files via upload

---
+++ Week 1/Day 2/Rectangular Love/Solution.java
@@ -0,0 +1,195 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+
+    public static class Rectangle {
+
+        // coordinates of bottom left corner
+        private int leftX;
+        private int bottomY;
+    
+        // dimensions
+        private int width;
+        private int height;
+    
+        public Rectangle() {}
+
+        public Rectangle(int leftX, int bottomY, int width, int height) {
+            this.leftX = leftX;
+            this.bottomY = bottomY;
+            this.width  = width;
+            this.height = height;
+        }
+
+        public int getLeftX() {
+            return leftX;
+        }
+
+        public int getBottomY() {
+            return bottomY;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(left: %d, bottom: %d, width: %d, height: %d)",
+                leftX, bottomY, leftX + width, bottomY + height);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) {
+                return true;
+            }
+            if (!(o instanceof Rectangle)) {
+                return false;
+            }
+            final Rectangle r = (Rectangle) o;
+            return leftX == r.leftX && bottomY == r.bottomY
+                   && width == r.width && height == r.height;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 17;
+            result = result * 31 + leftX;
+            result = result * 31 + bottomY;
+            result = result * 31 + width;
+            result = result * 31 + height;
+            return result;
+        }
+    }
+
+    public static Rectangle findRectangularOverlap(Rectangle rect1, Rectangle rect2) {
+
+        int x1 = rect1.getLeftX();
+        int x2 = rect2.getLeftX();
+        int y1 = rect1.getBottomY();
+        int y2 = rect2.getBottomY();
+        int length1 = rect1.getHeight();
+        int length2 = rect2.getHeight();
+        int width1 = rect1.getWidth();
+        int width2 = rect2.getWidth();
+        int xi1, xi2,yi1,yi2,xf,yf,lengthf,widthf;
+        xi1 = Math.max(x1, x2);
+        xi2 = Math.min(x1+width1, x2+width2);
+        if (xi1 >= xi2){
+            xf = 0;
+            widthf = 0;
+        }
+        else{
+            xf = xi1;
+            widthf = xi2-xi1;
+        }
+        yi1 = Math.max(y1, y2);
+        yi2 = Math.min(y1+length1, y2+length2);
+        if (yi1 >= yi2){
+            yf = 0;
+            lengthf = 0;
+        }
+        else{
+            yf = yi1;
+            lengthf = yi2-yi1;
+        }
+        if (lengthf == 0 || widthf == 0)
+            return new Rectangle();
+        return new Rectangle(xf,yf,widthf,lengthf);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void overlapAlongBothAxesTest() {
+        final Rectangle actual = findRectangularOverlap(
+            new Rectangle(1, 1, 6, 3), new Rectangle(5, 2, 3, 6));
+        final Rectangle expected = new Rectangle (5, 2, 2, 2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void oneRectangleInsideAnotherTest() {
+        final Rectangle actual = findRectangularOverlap(
+            new Rectangle(1, 1, 6, 6), new Rectangle(3, 3, 2, 2));
+        final Rectangle expected = new Rectangle(3, 3, 2, 2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void bothRectanglesTheSameTest() {
+        final Rectangle actual = findRectangularOverlap(
+            new Rectangle(2, 2, 4, 4), new Rectangle(2, 2, 4, 4));
+        final Rectangle expected = new Rectangle(2, 2, 4, 4);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void touchOnHorizontalEdgeTest() {
+        final Rectangle actual = findRectangularOverlap(
+            new Rectangle(1, 2, 3, 4), new Rectangle(2, 6, 2, 2));
+        final Rectangle expected = new Rectangle();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void touchOnVerticalEdgeTest() {
+        final Rectangle actual = findRectangularOverlap(
+            new Rectangle(1, 2, 3, 4), new Rectangle(4, 3, 2, 2));
+        final Rectangle expected = new Rectangle();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void touchAtCornerTest() {
+        final Rectangle actual = findRectangularOverlap(
+            new Rectangle(1, 1, 2, 2), new Rectangle(3, 3, 2, 2));
+        final Rectangle expected = new Rectangle();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void noOverlapTest() {
+        final Rectangle actual = findRectangularOverlap(
+            new Rectangle(1, 1, 2, 2), new Rectangle(4, 6, 3, 6));
+        final Rectangle expected = new Rectangle();
+        assertEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
